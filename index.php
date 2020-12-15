7f958559d36c0a4b2e9560b3bbc0d4fc12986710
ivanovicdenis <ivanovicdenis85@gmail.com>
2020-12-15 14:07:26 +0100

    Initial commit

--- index.php
+++ index.php
@@ -72,6 +72,11 @@ $db = "ruapdb";
  catch(Exception $e){
  die(var_dump($e));
  }
+ 
+ $sql_create = "CREATE TABLE registration_tbl(id INT NOT NULL
+AUTO_INCREMENT, PRIMARY KEY(id), name VARCHAR(30), email
+VARCHAR(30), date DATE);
+";
 
 
 // Insert registration info
